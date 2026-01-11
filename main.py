d65908393de4d8729ad0de59ca78a457b859b8a0
Rushikesh Shinde <rushikeshshinde760@gmail.com>
2026-01-11 13:35:54 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI 
+from transformers import pipeline
+from pydantic import BaseModel
+
+# for sql
+from datetime import datetime
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Load the sentiment analysis model
+classifier = pipeline("sentiment-analysis")
+
+class SentimentRequest(BaseModel):
+    text: str
+
+# Create database connection 
+DATABASE_URL = "sqlite:///./sentiment.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+# Create a session factory (to talk to DB)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# base class for our table
+Base = declarative_base()
+
+# define our table 
+class PredictionLog(Base):
+    __tablename__ = "predictions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String)
+    label = Column(String)
+    score = Column(Float)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+
+# create the table in database
+Base.metadata.create_all(bind=engine)
+
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Hello AI"}
+
+@app.post("/predict")
+def predict(request: SentimentRequest):
+    # We access the data using request.text
+    ai_result = classifier(request.text)
+    
+    #prepare data for DB
+    db_entry = PredictionLog(
+        text=request.text,
+        label=ai_result[0]['label'],
+        score=ai_result[0]['score']
+    )
+
+    # save to db
+    db = SessionLocal()     # open connection
+    try:
+        db.add(db_entry) # add to staging
+        db.commit()      # save changes
+        db.refresh(db_entry) # get the generated id
+    finally:
+        db.close()  # close the connection (Very important)
+    
+    return ai_result
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+# sentiment-api
+
+## Tech stack 
+
+- Web Framework: Fast API
+- Server: Uvicorn
+- AI library: transformers (by Hugging Face)
+
